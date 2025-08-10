ae4fb602e4fbaf6902a9cb5b683aa6990fb5ea88
Letjobana <mahlodimasite@gmail.com>
2025-08-10 16:08:04 +0200

    Added a job service class

---
+++ SageApi/SageService.Domain/Interfaces/ICourseInvoiceJobService.cs
@@ -0,0 +1,15 @@
+﻿namespace SageService.Domain.Interfaces
+{
+    /// <summary>
+    /// Handles the process of generating invoices for an entire course's learners.
+    /// Intended to run in background jobs.
+    /// </summary>
+    public interface ICourseInvoiceJobService
+    {
+        // <summary>
+        /// Enqueues a background job to create invoices for all learners in the given course.
+        /// Returns the background job ID.
+        /// </summary>
+        Task<string> EnqueueCourseInvoicesAsync(int courseId);
+    }
+}
